78846f62cdf8c00a120e41fa19d45b110f8804ae
mkyo2012 <hoanglam10499@gmail.com>
2019-07-27 16:14:15 +0700

    change color

--- android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerModule.java
@@ -82,7 +82,7 @@ class PickerModule extends ReactContextBaseJavaModule implements ActivityEventLi
     private ReadableMap options;
 
     //Grey 800
-    private final String DEFAULT_TINT = "#424242";
+    private final String DEFAULT_TINT = "#424240";
     private String cropperActiveWidgetColor = DEFAULT_TINT;
     private String cropperStatusBarColor = DEFAULT_TINT;
     private String cropperToolbarColor = DEFAULT_TINT;
